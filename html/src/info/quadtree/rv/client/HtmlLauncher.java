39ce6071c886b884b7a24199c18932347fef31ff
Quadtree <quadtree@gmail.com>
2017-04-30 00:13:26 -0700

    Size adjustment

--- html/src/info/quadtree/rv/client/HtmlLauncher.java
+++ html/src/info/quadtree/rv/client/HtmlLauncher.java
@@ -3,17 +3,18 @@ package info.quadtree.rv.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
 import info.quadtree.rv.RobotVillageGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+	@Override
+	public ApplicationListener createApplicationListener() {
+		return new RobotVillageGame();
+	}
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new RobotVillageGame();
-        }
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		return new GwtApplicationConfiguration(1024, 768);
+	}
 }
