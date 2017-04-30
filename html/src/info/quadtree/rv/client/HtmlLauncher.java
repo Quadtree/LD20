b480cf313a79527bd68e534f28da36d7cd86c814
Quadtree <quadtree@gmail.com>
2017-04-29 18:30:44 -0700

    Switched to libGDX

---
+++ html/src/info/quadtree/rv/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package info.quadtree.rv.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import info.quadtree.rv.RobotVillageGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new RobotVillageGame();
+        }
+}
