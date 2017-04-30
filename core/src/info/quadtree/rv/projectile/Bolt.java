9cf3d0455c36fb80c2a77f121815b6afbb76878d
Quadtree <quadtree@gmail.com>
2017-04-29 19:36:51 -0700

    More fixes

--- core/src/info/quadtree/rv/projectile/Bolt.java
+++ core/src/info/quadtree/rv/projectile/Bolt.java
@@ -1,61 +1,76 @@
 package info.quadtree.rv.projectile;
 
-import com.badlogic.gdx.physics.box2d.collision.FilterData;
-import com.badlogic.gdx.physics.box2d.collision.shapes.CircleDef;
-import com.badlogic.gdx.physics.box2d.common.Vector2;
-import com.badlogic.gdx.physics.box2d.dynamics.BodyDef;
-import com.badlogic.gdx.physics.box2d.dynamics.ContactListener;
-import com.badlogic.gdx.physics.box2d.dynamics.contacts.ContactPoint;
-import com.badlogic.gdx.physics.box2d.dynamics.contacts.ContactResult;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 
 import info.quadtree.rv.Game;
+import info.quadtree.rv.Spark;
 import info.quadtree.rv.actor.Harmable;
 import info.quadtree.rv.actor.PhysicalActor;
 import info.quadtree.rv.graphics.SGF;
-import sv.vfx.Spark;
 
 public class Bolt extends PhysicalActor implements ContactListener {
-	
-	String graphic;
-	
-	boolean destroyed = false;
-	
+
 	float damage;
-	
-	public Bolt(float x, float y, float angle, float speed, String graphic, boolean hostile, float damage)
-	{
+
+	boolean destroyed = false;
+
+	String graphic;
+
+	public Bolt(float x, float y, float angle, float speed, String graphic, boolean hostile, float damage) {
 		BodyDef bd = new BodyDef();
-		
+
 		bd.position.x = x;
 		bd.position.y = y;
 		bd.angle = angle;
-		
+
 		body = Game.s.physicsWorld.createBody(bd);
-		
-		CircleDef cd = new CircleDef();
-		cd.radius = 0.2f;
-		cd.density = 1;
-		
-		FilterData fd = new FilterData();
+
+		CircleShape cd = new CircleShape();
+		cd.setRadius(0.2f);
+
+		FixtureDef fxd = new FixtureDef();
+		fxd.shape = cd;
+		fxd.density = 1;
+
+		Filter fd = new Filter();
 		fd.categoryBits = 1 << 5;
-		fd.maskBits = 1 << (hostile ? 2 : 3) | (1) | (!hostile ? (1 << 7) : 0);
-		
-		body.createShape(cd).setFilterData(fd);
-		
-		body.setMassFromShapes();
-		
-		body.setLinearVelocity(new Vector2((float)Math.cos(angle), (float)Math.sin(angle)).mul(speed));
-		
+		fd.maskBits = (short) (1 << (hostile ? 2 : 3) | (1) | (!hostile ? (1 << 7) : 0));
+
+		body.createFixture(fxd).setFilterData(fd);
+
+		body.setLinearVelocity(new Vector2((float) Math.cos(angle), (float) Math.sin(angle)).scl(speed));
+
 		body.setUserData(this);
-		
+
 		this.graphic = graphic;
 		this.damage = damage;
 	}
 
 	@Override
-	public void render() {
-		SGF.getInstance().renderImage(graphic, body.getPosition().x, body.getPosition().y, -1/32.f, -1/32.f, body.getAngle(), true);
-		super.render();
+	public void beginContact(Contact contact) {
+		destroyed = true;
+
+		Object o1 = contact.getFixtureA().getBody().getUserData();
+		Object o2 = contact.getFixtureB().getBody().getUserData();
+
+		if (o1 instanceof Harmable)
+			((Harmable) o1).takeDamage(damage);
+		if (o2 instanceof Harmable)
+			((Harmable) o2).takeDamage(damage);
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -64,40 +79,33 @@ public class Bolt extends PhysicalActor implements ContactListener {
 	}
 
 	@Override
-	public void add(ContactPoint point) {
-		destroyed = true;
-		
-		Object o1 = point.shape1.getBody().getUserData();
-		Object o2 = point.shape2.getBody().getUserData();
-		
-		if(o1 instanceof Harmable) ((Harmable) o1).takeDamage(damage);
-		if(o2 instanceof Harmable) ((Harmable) o2).takeDamage(damage);
-	}
-
-	@Override
 	public void notifyDestroyed() {
-		
-		if(!graphic.equals("shockwave"))
-			for(int i=0;i<Math.min(4 * damage, 16);i++)
-			{
+
+		if (!graphic.equals("shockwave"))
+			for (int i = 0; i < Math.min(4 * damage, 16); i++) {
 				Game.s.actors.add(new Spark(body.getPosition().x, body.getPosition().y, 0.35f, 0.015f, 7.f, graphic));
 			}
-		
+
 		Game.s.playAudio("bolthit");
-		
+
 		super.notifyDestroyed();
 	}
 
 	@Override
-	public void persist(ContactPoint point) {
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
-	public void remove(ContactPoint point) {
+	public void preSolve(Contact contact, Manifold oldManifold) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
-	public void result(ContactResult point) {
-		
+	public void render() {
+		SGF.getInstance().renderImage(graphic, body.getPosition().x, body.getPosition().y, -1 / 32.f, -1 / 32.f, body.getAngle(), true);
+		super.render();
 	}
 }
--- core/src/info/quadtree/rv/projectile/BreakerShot.java
+++ core/src/info/quadtree/rv/projectile/BreakerShot.java
@@ -1,51 +1,53 @@
 package info.quadtree.rv.projectile;
 
-import com.badlogic.gdx.physics.box2d.collision.FilterData;
-import com.badlogic.gdx.physics.box2d.collision.shapes.CircleDef;
-import com.badlogic.gdx.physics.box2d.common.Vector2;
-import com.badlogic.gdx.physics.box2d.dynamics.BodyDef;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
 import info.quadtree.rv.Game;
 import info.quadtree.rv.actor.PhysicalActor;
 import info.quadtree.rv.graphics.SGF;
 
 public class BreakerShot extends PhysicalActor {
-	public BreakerShot(float x, float y, float angle)
-	{
+	public BreakerShot(float x, float y, float angle) {
 		BodyDef bd = new BodyDef();
-		
+
 		bd.position.x = x;
 		bd.position.y = y;
 		bd.angle = angle;
 		bd.linearDamping = 0.4f;
-		
+
 		body = Game.s.physicsWorld.createBody(bd);
-		
-		CircleDef cd = new CircleDef();
-		cd.radius = 0.25f;
-		cd.density = 10;
-		
-		FilterData fd = new FilterData();
+
+		CircleShape cd = new CircleShape();
+		cd.setRadius(0.25f);
+
+		FixtureDef fxd = new FixtureDef();
+		fxd.density = 10;
+		fxd.shape = cd;
+
+		Filter fd = new Filter();
 		fd.categoryBits = 1 << 5;
 		fd.maskBits = (1 << 3) | (1) | (1 << 7);
-		
-		body.createShape(cd).setFilterData(fd);
-		
-		body.setMassFromShapes();
-		
-		body.setLinearVelocity(new Vector2((float)Math.cos(angle), (float)Math.sin(angle)).mul(24));
-		
+
+		body.createFixture(fxd).setFilterData(fd);
+
+		body.setLinearVelocity(new Vector2((float) Math.cos(angle), (float) Math.sin(angle)).scl(24));
+
 		body.setUserData(this);
+	}
+
+	@Override
+	public boolean keep() {
+		return body.getLinearVelocity().len() > 2.5f;
 	}
 
 	@Override
 	public void render() {
 		SGF.getInstance().renderImage("breakershot", body.getPosition().x, body.getPosition().y, 0.5f, 0.5f, body.getAngle(), true);
 		super.render();
-	}
-
-	@Override
-	public boolean keep() {
-		return body.getLinearVelocity().length() > 2.5f;
 	}
 
 }
